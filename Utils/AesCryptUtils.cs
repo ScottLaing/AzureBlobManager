37eaa2be43813756d8495c47510981fdb6178c2e
Scott Laing <scottlaing@gmail.com>
2024-05-12 16:01:22 -0500

    Code improvements to aes wrapper class.

--- Utils/AesCryptUtils.cs
+++ Utils/AesCryptUtils.cs
@@ -8,10 +8,20 @@ namespace AzureBlobManager.Utils
     public class AesCryptUtils
     {
         private const string defaultKey = "76AA6E93-AB72-4B33-B382-ABF77FF64C83"; // Must be at least 16 characters
-        private static readonly string defaultIv = "55DCB9FA-32DB-4E93-A7BE-BF4EDBE29C42".Substring(0,16); // Must be 16 characters
+        private static readonly string defaultIv = "55DCB9FA-32DB-4E"; // Must be 16 characters
+        private const string KeyAndSaltMustNotBeEmpty = "Key and salt must not be empty.";
+        private const string SaltMustBeAtLeast16Characters = "Salt must be at least 16 characters.";
+        private const string KeyMustBe16CharactersLong = "Key must be 16 characters long.";
+        private const string EncryptedTextMustNotBeBlank = "encryptedText must not be blank.";
+        private const string PlainTextMustNotBeBlank = "plainText must not be blank.";
 
         public static string EncryptString(string plainText)
         {
+            if (string.IsNullOrWhiteSpace(plainText))
+            {
+                throw new ArgumentNullException(PlainTextMustNotBeBlank);
+            }
+
             byte[] plainTextBytes = Encoding.UTF8.GetBytes(plainText);
 
             using (var aes = Aes.Create())
@@ -34,8 +44,57 @@ namespace AzureBlobManager.Utils
             }
         }
 
+        public static string EncryptString(string plainText, string key, string salt)
+        {
+            if (string.IsNullOrWhiteSpace(plainText))
+            {
+                throw new ArgumentNullException(PlainTextMustNotBeBlank);
+            }
+
+            if (string.IsNullOrWhiteSpace(key) || string.IsNullOrWhiteSpace(salt))
+            {
+                throw new ArgumentNullException(KeyAndSaltMustNotBeEmpty);
+            }
+
+            if (salt.Length < 16)
+            {
+                throw new ArgumentException(SaltMustBeAtLeast16Characters);
+            }
+
+            if (key.Length != 16)
+            {
+                throw new ArgumentException(KeyMustBe16CharactersLong);
+            }
+
+            byte[] plainTextBytes = Encoding.UTF8.GetBytes(plainText);
+
+            using (var aes = Aes.Create())
+            {
+                aes.Key = Encoding.UTF8.GetBytes(key);
+                aes.IV = Encoding.UTF8.GetBytes(salt);
+                aes.Mode = CipherMode.CBC; // Set the cipher mode (CBC is common)
+
+                using (var encryptor = aes.CreateEncryptor())
+                {
+                    using (var memoryStream = new MemoryStream())
+                    {
+                        using (var cryptoStream = new CryptoStream(memoryStream, encryptor, CryptoStreamMode.Write))
+                        {
+                            cryptoStream.Write(plainTextBytes, 0, plainTextBytes.Length);
+                        }
+                        return Convert.ToBase64String(memoryStream.ToArray());
+                    }
+                }
+            }
+        }
+
         public static string DecryptString(string encryptedText)
         {
+            if (string.IsNullOrWhiteSpace(encryptedText))
+            {
+                throw new ArgumentNullException(EncryptedTextMustNotBeBlank);
+            }
+
             byte[] encryptedBytes = Convert.FromBase64String(encryptedText);
 
             using (var aes = Aes.Create())
@@ -60,53 +119,27 @@ namespace AzureBlobManager.Utils
             }
         }
 
-        public static string EncryptString2(string plainText, string key, string salt)
+        public static string DecryptString(string encryptedText, string key, string salt)
         {
-            if (string.IsNullOrWhiteSpace(plainText)) throw new ArgumentNullException("plainText");
+            if (string.IsNullOrWhiteSpace(encryptedText))
+            {
+                throw new ArgumentNullException(EncryptedTextMustNotBeBlank);
+            }
 
             if (string.IsNullOrWhiteSpace(key) || string.IsNullOrWhiteSpace(salt))
             {
-                throw new ArgumentNullException("Key and salt must not be empty.");
+                throw new ArgumentNullException(KeyAndSaltMustNotBeEmpty);
             }
 
-            if (salt.Length < 16) throw new ArgumentException("Salt must be at least 16 characters.");
-
-            if (key.Length != 16) throw new ArgumentException("Key must be 16 characters long.");
-
-            byte[] plainTextBytes = Encoding.UTF8.GetBytes(plainText);
-
-            using (var aes = Aes.Create())
+            if (salt.Length < 16)
             {
-                aes.Key = Encoding.UTF8.GetBytes(key);
-                aes.IV = Encoding.UTF8.GetBytes(salt);
-                aes.Mode = CipherMode.CBC; // Set the cipher mode (CBC is common)
-
-                using (var encryptor = aes.CreateEncryptor())
-                {
-                    using (var memoryStream = new MemoryStream())
-                    {
-                        using (var cryptoStream = new CryptoStream(memoryStream, encryptor, CryptoStreamMode.Write))
-                        {
-                            cryptoStream.Write(plainTextBytes, 0, plainTextBytes.Length);
-                        }
-                        return Convert.ToBase64String(memoryStream.ToArray());
-                    }
-                }
+                throw new ArgumentException(SaltMustBeAtLeast16Characters);
             }
-        }
 
-        public static string DecryptString2(string encryptedText, string key, string salt)
-        {
-            if (string.IsNullOrWhiteSpace(encryptedText)) throw new ArgumentNullException("encryptedText");
-
-            if (string.IsNullOrWhiteSpace(key) || string.IsNullOrWhiteSpace(salt))
+            if (key.Length != 16)
             {
-                throw new ArgumentNullException("Key and salt must not be empty.");
+                throw new ArgumentException(KeyMustBe16CharactersLong);
             }
-
-            if (salt.Length < 16) throw new ArgumentException("Salt must be at least 16 characters.");
-
-            if (key.Length != 16) throw new ArgumentException("Key must be 16 characters long.");
 
             byte[] encryptedBytes = Convert.FromBase64String(encryptedText);
 
