a2e868a0e38ae8b628978b59c20ec6a9a65f3c31
Scott Laing <scottlaing@gmail.com>
2024-05-24 23:39:26 -0500

    Work on new blob item change window (blob item editting behaviors).

---
+++ Windows/BlobItemChangeWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using SimpleBlobUtility.Dtos;
+using System.Collections.Generic;
+using System.Windows;
+
+
+namespace SimpleBlobUtility.Windows
+{
+    /// <summary>
+    /// Interaction logic for BlobMetadataWindow.xaml
+    /// </summary>
+    public partial class BlobItemChangeWindow : Window
+    {
+        public List<MetadataDto> SourceCollection = new List<MetadataDto>();
+        public bool DialogWasSaved = false;
+
+        public App? App =>  Application.Current as App;
+
+        public BlobItemChangeWindow(string fileName, List<MetadataDto> sourceCollection)
+        {
+            InitializeComponent();
+
+            //dgMetadataList.ItemsSource = sourceCollection;
+            this.txtBlobName.Text = fileName;
+
+            SourceCollection = sourceCollection;
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            SourceCollection.Add(new MetadataDto()
+            {
+                KeyName = "New Key",
+                Value = "New Value"
+            });
+            //dgMetadataList.ItemsSource = null;
+            //dgMetadataList.ItemsSource = SourceCollection;
+        }
+
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            DialogWasSaved = true;
+            this.DialogResult = true;
+            this.Close();
+        }
+    }
+}
