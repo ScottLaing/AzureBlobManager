93d570a0767ef5b63a33586f2479d6fab6ae4286
Scott Laing <scottlaing@gmail.com>
2024-07-26 23:05:09 -0500

    Misc UI improvements, and added new button image to add sample text button on encrypt window.

---
+++ AzureBlobManager.WPF/Controls/AddSampleText.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace AzureBlobManager.Controls
+{
+    /// <summary>
+    /// Interaction logic for AddSampleTextViewbox.xaml
+    /// </summary>
+    public partial class AddSampleTextViewbox : UserControl
+    {
+        public AddSampleTextViewbox()
+        {
+            InitializeComponent();
+        }
+    }
+}
