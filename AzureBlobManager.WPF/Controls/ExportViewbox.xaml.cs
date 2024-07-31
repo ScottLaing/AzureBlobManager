ccdfa3b91ebe6be6e5992c6bcd3f42f854cbcf28
Scott Laing <scottlaing@gmail.com>
2024-07-30 23:16:04 -0500

    UI improvements to encrypt window, import and export buttons.

---
+++ AzureBlobManager.WPF/Controls/ExportViewbox.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace AzureBlobManager.Controls
+{
+    /// <summary>
+    /// Interaction logic for AddViewbox.xaml
+    /// </summary>
+    public partial class ExportViewbox : UserControl
+    {
+        public ExportViewbox()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ AzureBlobManager.WPF/Controls/ImportViewbox.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace AzureBlobManager.Controls
+{
+    /// <summary>
+    /// Interaction logic for AddViewbox.xaml
+    /// </summary>
+    public partial class ImportViewbox : UserControl
+    {
+        public ImportViewbox()
+        {
+            InitializeComponent();
+        }
+    }
+}
